26173be7b85ff15577fbf8be7211827d1d7ca739
SoraLinken <83252345+SoraLinken@users.noreply.github.com>
2024-05-27 21:29:06 +0300

    add experience schema

---
+++ models/Experience.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ExperienceSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Participant',
+    required: true
+  }],
+  synchrony: {
+    type: Map,
+    of: new mongoose.Schema({
+      timestamp: Date,
+      data: mongoose.Schema.Types.Mixed
+    }, { _id: false }),
+    required: true
+  },
+  notes: {
+    type: String,
+    default: ''
+  },
+  feedbackParticipant1: {
+    type: String,
+    default: ''
+  },
+  feedbackParticipant2: {
+    type: String,
+    default: ''
+  }
+});
+
+module.exports = mongoose.model('Experience', ExperienceSchema);
